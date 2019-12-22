925b3596f9b236070bf98e7a4e2ffe5342af847a
Keagan <esorts.k990@gmail.com>
2019-12-21 22:46:57 -0500

    first api call

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+
+import Goals from "./component/Goals"
+
+function App() {
+  return (
+    <div className="App">
+      
+       
+        <Goals />
+     
+    </div>
+  );
+}
+
+export default App;
